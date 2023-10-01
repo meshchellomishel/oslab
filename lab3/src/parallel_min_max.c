de1561d6e249fdcb16ebc7b1a20c7747d8f87231
Mikhail <126414636+meshchellomishel@users.noreply.github.com>
2023-10-01 11:46:32 +0000

    Almost done lab3

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -5,138 +5,431 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <errno.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #include <getopt.h>
 
 #include "find_min_max.h"
 #include "utils.h"
 
-int main(int argc, char **argv) {
-  int seed = -1;
-  int array_size = -1;
-  int pnum = -1;
-  bool with_files = false;
+#define MAX_PNUM 10
 
-  while (true) {
-    int current_optind = optind ? optind : 1;
+struct args {
+  int seed;
+  int array_size;
+  int pnum;
+  bool with_files;
+};
 
-    static struct option options[] = {{"seed", required_argument, 0, 0},
-                                      {"array_size", required_argument, 0, 0},
-                                      {"pnum", required_argument, 0, 0},
-                                      {"by_files", no_argument, 0, 'f'},
-                                      {0, 0, 0, 0}};
+struct process {
+  int id;
+  int fd[2];
+  int pid;
+  int read_fd;
+  int write_fd;
+  char *filepath;
+};
 
-    int option_index = 0;
-    int c = getopt_long(argc, argv, "f", options, &option_index);
+struct ctx {
+  struct args args;
+  int *array;
+  struct MinMax info;
+  struct process *processes;
+};
 
-    if (c == -1) break;
+void generate_filepath_by_id(char *str, int id)
+{
+  sprintf(str, "./lab-fd-%d", id);
+}
 
-    switch (c) {
-      case 0:
-        switch (option_index) {
-          case 0:
-            seed = atoi(optarg);
-            // your code here
-            // error handling
-            break;
-          case 1:
-            array_size = atoi(optarg);
-            // your code here
-            // error handling
-            break;
-          case 2:
-            pnum = atoi(optarg);
-            // your code here
-            // error handling
-            break;
-          case 3:
-            with_files = true;
-            break;
+void process_init(struct process *process)
+{
+  process->filepath = NULL;
+}
 
-          defalut:
-            printf("Index %d is out of options\n", option_index);
-        }
-        break;
-      case 'f':
-        with_files = true;
-        break;
+int process_alloc(struct process *process, bool with_files)
+{
+  int fd;
 
-      case '?':
-        break;
-
-      default:
-        printf("getopt returned character code 0%o?\n", c);
+  if (with_files) {
+    process->filepath = malloc(sizeof(char *) * 256);
+    if (!process->filepath) {
+      printf("Can`t malloc a filepath\n");
+      return -1;
     }
+    generate_filepath_by_id(process->filepath, process->id);
+    printf("INFO: Child file is %s\n", process->filepath);
+
+    fd = open(process->filepath, O_CREAT, O_RDWR);
+    if (fd < 0) {
+      printf("ERROR: Can`t open a file\n");
+      return -1;
+    }
+    process->read_fd = fd;
+    process->write_fd = fd;
+  } else {
+    pipe(process->fd);
+    process->read_fd = process->fd[0];
+    process->write_fd = process->fd[1];
+  }
+
+  return 0;
+}
+
+void process_destroy(struct process *process)
+{
+    close(process->read_fd);
+    close(process->write_fd);
+}
+
+void ctx_init(struct ctx *ctx)
+{
+  ctx->array = NULL;
+  ctx->processes = NULL;
+}
+
+int ctx_alloc(struct ctx *ctx)
+{
+  ctx->array = malloc(sizeof(int) * ctx->args.array_size);
+  if (!ctx->array)
+    return -1;
+
+  ctx->processes = malloc(sizeof(struct process) * ctx->args.pnum);
+  if (!ctx->processes)
+    return -1;
+
+  ctx->info.min = INT_MAX;
+  ctx->info.max = INT_MIN;
+  return 0;
+}
+
+int ctx_processes_alloc(struct ctx *ctx)
+{
+  int ret;
+
+  for (int i = 0;i < ctx->args.pnum;i++) {
+    struct process *process = &ctx->processes[i];
+    process_init(process);
+    process->id = i;
+
+    ret = process_alloc(process, ctx->args.with_files);
+    if (ret < 0) {
+      printf("ERROR: Process allocate failed\n");
+      return -1;
+    }
+  }
+}
+
+int ctx_destroy(struct ctx *ctx) 
+{
+  int ret;
+
+  if (ctx->processes) {
+    for (int i = 0; i < ctx->args.pnum; i++) {
+      struct process *process = &ctx->processes[i];
+
+      if (process) {
+        process_destroy(process);
+        if (ctx->args.with_files) {
+          if (process->filepath) {
+            ret = remove(process->filepath);
+            if (ret < 0) {
+              printf("ERROR: Can`t delete file '%s'", process->filepath);
+              return -1;
+            }
+            free(process->filepath);
+          }
+        }
+      }
+    }
+  }
+  if (ctx->array) {
+    free(ctx->array);
+  }
+  if (ctx->processes)
+    free(ctx->processes);
+}
+
+void on_error(struct ctx *ctx, int error)
+{
+  ctx_destroy(ctx);
+  exit(error);
+}
+
+int parse_args(struct ctx *ctx, int argc, char **argv)
+{
+    while (true) {
+      int current_optind = optind ? optind : 1;
+
+      static struct option options[] = {{"seed", required_argument, 0, 0},
+                                        {"array_size", required_argument, 0, 0},
+                                        {"pnum", required_argument, 0, 0},
+                                        {"by_files", no_argument, 0, 'f'},
+                                        {0, 0, 0, 0}};
+
+      int option_index = 0;
+      int c = getopt_long(argc, argv, "f", options, &option_index);
+
+      if (c == -1) break;
+
+      switch (c) {
+        case 0:
+          switch (option_index) {
+            case 0:
+              ctx->args.seed = atoi(optarg);
+              if (ctx->args.seed <= 0) {
+                printf("seed is a positive number\n");
+                return -1;
+              }
+              break;
+            case 1:
+              ctx->args.array_size = atoi(optarg);
+              if (ctx->args.array_size <= 0) {
+                printf("array_size is a positive number\n");
+                return -1;
+              }
+              break;
+            case 2:
+              ctx->args.pnum = atoi(optarg);
+              if (ctx->args.pnum <= 0 || ctx->args.pnum >= MAX_PNUM) {
+                printf("pnum is a positive number\n");
+                return -1;
+              }
+              break;
+            case 3:
+              ctx->args.with_files = true;
+              break;
+
+            defalut:
+              printf("Index %d is out of options\n", option_index);
+          }
+          break;
+        case 'f':
+          ctx->args.with_files = true;
+          break;
+
+        case '?':
+          break;
+
+        default:
+          printf("getopt returned character code 0%o?\n", c);
+      }
   }
 
   if (optind < argc) {
     printf("Has at least one no option argument\n");
-    return 1;
+    return -1;
   }
 
-  if (seed == -1 || array_size == -1 || pnum == -1) {
+  if (!ctx->args.seed || !ctx->args.array_size || !ctx->args.pnum) {
     printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
            argv[0]);
-    return 1;
+    return -1;
   }
 
-  int *array = malloc(sizeof(int) * array_size);
-  GenerateArray(array, array_size, seed);
+  return 0;
+}
+
+int do_fork(int *array, struct process *process, int begin, int end)
+{
+  pid_t child_pid = fork();
+  if (child_pid >= 0) {
+    if (child_pid == 0) {
+      // child process
+      int ret;
+      struct MinMax min_max;
+
+      min_max = GetMinMax(array, begin, end);
+
+      printf("INFO: Child result is {%d, %d}\n", min_max.min, min_max.max);
+
+      ret = write(process->write_fd, &min_max, sizeof(struct MinMax));
+      if (ret < 0) {
+        printf("ERROR: Can`t write to fd\n");
+        exit(-1);
+      }
+      exit(0);
+      
+    } else {
+      printf("INFO: Child PID is %d\n", child_pid);
+      process->pid = child_pid;
+    }
+
+  } else {
+    printf("Fork failed!\n");
+    return -1;
+  }
+
+  return 0;
+}
+
+int read_from_processes(struct ctx *ctx)
+{
+  int ret;
+  struct MinMax min_max;
+  printf("INFO: Reading from processes...\n");
+
+  for (int i = 0; i < ctx->args.pnum; i++) {
+    ret = read(ctx->processes[i].read_fd, &min_max, sizeof(struct MinMax));
+    if (ret < 0)
+      return -1;
+    
+    printf("INFO: child recv {%d, %d}\n", min_max.min, min_max.max);
+
+    if (min_max.min < ctx->info.min) ctx->info.min = min_max.min;
+    if (min_max.max > ctx->info.max) ctx->info.max = min_max.max;
+  }
+}
+
+int do_parallel(int *array, struct process *process, int begin, int end, bool with_files)
+{
+  int ret;
+
+  printf("DEBUG: begin = %d, end = %d\n", begin, end);
+
+  ret = do_fork(array, process, begin, end);
+  if (ret < 0) {
+    printf("ERROR: do_fork failed\n");
+    return -1;
+  }
+  printf("INFO: fork done\n");
+
+  return 0;
+}
+
+int distribute_parallel(struct ctx *ctx, int chunk)
+{
+  int begin, end, ret;
+  int active_child_processes = 0;
+  bool with_files = ctx->args.with_files;
+  
+  if (chunk == 0)
+    chunk = ctx->args.array_size;
+
+  begin = 0;
+  end = 0;
+  for (int i = 0;i < ctx->args.pnum - 1;i++) {
+    if (active_child_processes == i) {
+      begin = end;
+      end += chunk;
+    }
+
+    struct process *process = &ctx->processes[i];
+    process->id = i;
+    
+    ret = do_parallel(ctx->array, process, begin, end, with_files);
+    if (ret < 0) {
+      printf("ERROR: do_parallel failed\n");
+      continue;
+    }
+    active_child_processes += 1;
+  }
+
+  if (end - begin) {
+    begin = end;
+    end = ctx->args.array_size;
+
+    struct process *process = &ctx->processes[active_child_processes];
+    process->id = active_child_processes;
+
+    ret = do_parallel(ctx->array, process, begin, end, with_files);
+    if (ret < 0) {
+      printf("ERROR: do_parallel failed\n");
+      return -1;
+    }
+
+    active_child_processes += 1;
+  }
+
+  return active_child_processes;
+}
+
+int parallel(struct ctx *ctx, int chunk)
+{
+  int ret;
   int active_child_processes = 0;
 
-  struct timeval start_time;
-  gettimeofday(&start_time, NULL);
+  printf("INFO: Chunks...\n");
 
-  for (int i = 0; i < pnum; i++) {
-    pid_t child_pid = fork();
-    if (child_pid >= 0) {
-      // successful fork
-      active_child_processes += 1;
-      if (child_pid == 0) {
-        // child process
-
-        // parallel somehow
-
-        if (with_files) {
-          // use files here
-        } else {
-          // use pipe here
-        }
-        return 0;
-      }
-
-    } else {
-      printf("Fork failed!\n");
-      return 1;
-    }
+  active_child_processes = distribute_parallel(ctx, chunk);
+  if (active_child_processes <= 0) {
+    printf("ERROR: Failed to distribute processes\n");
+    return -1;
   }
 
-  while (active_child_processes > 0) {
-    // your code here
+  printf("INFO: Waiting processes...\n");
+  int status, can_continue;
+  for (int i = 0;i < ctx->args.pnum;i++) {
+    waitpid(ctx->processes[i].pid, &status, 0);
+    if (WEXITSTATUS(status) != 0) {
+      can_continue = 0;
+      printf("ERROR: Child with pid %d failed, exit status %d\n",
+             ctx->processes[i].pid, WEXITSTATUS(status));
+      printf("\t error message: %s\n", strerror(WEXITSTATUS(status)));
+    }
 
     active_child_processes -= 1;
   }
 
-  struct MinMax min_max;
-  min_max.min = INT_MAX;
-  min_max.max = INT_MIN;
+  if (!can_continue) {
+    printf("ERROR: Can`t can`t continue, child process failed\n");
+    on_error(ctx, errno);
+  }
 
-  for (int i = 0; i < pnum; i++) {
-    int min = INT_MAX;
-    int max = INT_MIN;
+  ret = read_from_processes(ctx);
+  if (ret < 0) {
+    printf("ERROR: Can`t read from processes");
+    on_error(ctx, errno);
+  }
 
-    if (with_files) {
-      // read from files
-    } else {
-      // read from pipes
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  int ret, chunk;
+  struct ctx ctx;
+
+  ctx_init(&ctx);
+
+  printf("INFO: Parsing args...\n");
+  ret = parse_args(&ctx, argc, argv);
+  if (ret < 0)
+    on_error(&ctx, -ret);
+  
+  printf("With files = %d\n", false);
+  
+  printf("INFO: Context alloc...\n");
+  ret = ctx_alloc(&ctx);
+  if (ret < 0)
+    on_error(&ctx, -ret);
+
+  printf("INFO: Processes alloc...\n");
+  ret = ctx_processes_alloc(&ctx);
+  if (ret < 0)
+    on_error(&ctx, -ret);
+
+  GenerateArray(ctx.array, ctx.args.array_size, ctx.args.seed);
+  PrintArray(ctx.array, ctx.args.array_size);
+
+  chunk = ctx.args.array_size / (ctx.args.pnum);
+
+  struct timeval start_time;
+  gettimeofday(&start_time, NULL);
+
+  if (chunk != 1) {
+    ret = parallel(&ctx, chunk);
+    if (ret < 0) {
+      on_error(&ctx, errno);
     }
 
-    if (min < min_max.min) min_max.min = min;
-    if (max > min_max.max) min_max.max = max;
+  } else {
+    struct MinMax min_max;
+
+    min_max = GetMinMax(ctx.array, 0, ctx.args.array_size);
   }
 
   struct timeval finish_time;
@@ -145,11 +438,11 @@ int main(int argc, char **argv) {
   double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
   elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
-  free(array);
-
-  printf("Min: %d\n", min_max.min);
-  printf("Max: %d\n", min_max.max);
+  printf("Min: %d\n", ctx.info.min);
+  printf("Max: %d\n", ctx.info.max);
   printf("Elapsed time: %fms\n", elapsed_time);
   fflush(NULL);
+  
+  ctx_destroy(&ctx);
   return 0;
 }
--- lab3/src/utils.c
+++ lab3/src/utils.c
@@ -8,3 +8,18 @@ void GenerateArray(int *array, unsigned int array_size, unsigned int seed) {
     array[i] = rand();
   }
 }
+
+void PrintArray(int *arr, int array_size)
+{
+  int N = array_size;
+
+  printf("[ ");
+  for (int i = 0; i < N - 1; i++) {
+      printf("%d, ", arr[i]);
+      if (start_skip(i, N)) {
+          printf("\t...\t");
+          i = N - PRINT_MAX_BOUND;
+      }
+  }
+  printf("%d ]\n", arr[N - 1]);
+}
